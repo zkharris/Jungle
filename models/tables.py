970a854ed8ea151b539db5fdf99d1aa48d2f8850
zacharyharris <zkharris@ucsc.edu>
2018-11-26 23:55:03 -0800

    initialize tables.py start pet table

---
+++ models/tables.py
@@ -0,0 +1,19 @@
+import datetime
+
+def get_user_email():
+    return None if auth.user is None else auth.user.email
+
+def get_current_time():
+    return datetime.datetime.utcnow()
+
+# Pet
+
+# TODO: store image(s) into this table
+db.define_table('pet',
+                Field('petName', 'text'),
+                Field('petDescription', default="No Description"),
+                Field('petType', 'text'),
+                Field('petOwnerEmail')
+                )
+
+
